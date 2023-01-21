07630f3de2b36801424f8340ef4051a9e61d76de
NguyenQuy9x <32302648+NguyenQuy98@users.noreply.github.com>
2023-01-21 13:09:48 +0700

    push code

--- happyNewYear/app.js
+++ happyNewYear/app.js
@@ -19,3 +19,43 @@ let x = setInterval(function stime() {
         window.location.href = "intro.html"
     }
 }, 0.1);
+
+var images = [
+    'kn_1.jpg', 'kn_1_1.jpg', 'kn_2.jpg', 'kn_3.jpg', 'kn_14.jpg', 'kn_5.jpg', 'kn_6.jpg', 'kn_7.jpg', 'kn_8.jpg', 'kn_9.jpg',
+    'kn_10.jpg', 'kn_11.jpg', 'kn_12.jpg', 'kn_13.jpg', 'kn_13_1.jpg', 'kn_14.jpg', 'kn_15.jpg', 'kn_16.jpg', 'kn_17.jpg', 'kn_18.jpg',
+    'kn_19.jpg', 'kn_20.jpg', 'kn_21.jpg', 'kn_22.jpg', 'kn_23.jpg', 'kn_24.jpg', 'kn_23_1.jpg', 'kn_24_1.jpg', 'kn_25.jpg', 'kn_26.jpg', 'kn_27.jpg',
+    'kn_28.jpg', 'kn_29.jpg', 'kn_30.jpg', 'kn_31.jpg', 'kn_32.jpg', 'kn_33.jpg', 'kn_34.jpg', 'kn_35.jpg', 'kn_36.jpg',
+    'kn_37.jpg', 'kn_38.jpg', 'kn_39.jpg', 'kn_40.jpg', 'kn_41.jpg', 'kn_42.jpg'
+];
+let set_1 = document.querySelectorAll('.set-radom');
+for (let i = 0; i < set_1.length; i++) {
+    let val_image = Math.floor(Math.random() * images.length);
+    document.getElementById("set-1-" + i).style.backgroundImage = 'url(image/' + images[val_image] + ')';
+}
+var timer = {
+    interval: null,
+    seconds: 20,
+
+    start: function () {
+        var self = this;
+
+        this.interval = setInterval(function () {
+            self.seconds--;
+
+            if (self.seconds == 0) {
+                for (let i = 0; i < set_1.length; i++) {
+                    let val_image = Math.floor(Math.random() * images.length);
+                    console.log(val_image);
+                    document.getElementById("set-1-" + i).style.backgroundImage = 'url(image/' + images[val_image] + ')';
+                }
+                self.seconds = 20;
+            }
+        }, 1000);
+    },
+
+    stop: function () {
+        window.clearInterval(this.interval)
+    }
+}
+
+timer.start();
